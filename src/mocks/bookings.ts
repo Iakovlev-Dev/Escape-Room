8e0dd82968383fc49e19c798dc7cdc947c99fd11
Iakovlev Igor <iakovlev.igor.dev@gmail.com>
2023-12-21 22:22:02 +0300

    Add props to booking

---
+++ src/mocks/bookings.ts
@@ -0,0 +1,75 @@
+
+export const booking = [
+  {
+    id: 'aba664c3-bdf3-4fb3-b8f3-42e007864bbf',
+    location: {
+      address: 'Набережная реки Карповки, 5П',
+    //   coords: [30.317359, 30.217359]
+    },
+    slots: {
+      today: [
+        {
+          time: '14:00',
+          isAvailable: false
+        },
+        {
+          time: '15:00',
+          isAvailable: true
+        },
+        {
+          time: '16:00',
+          isAvailable: false
+        },
+      ],
+      tomorrow: [
+        {
+          time: '14:00',
+          isAvailable: false
+        },
+        {
+          time: '15:00',
+          isAvailable: false
+        },
+        {
+          time: '16:00',
+          isAvailable: true
+        },
+      ]
+    }},
+  {
+    id: 'aba665c3-bdf3-4fb3-b8f3-42e007864bbf',
+    location: {
+      address: 'Глухая Зеленина 6',
+      //   coords: [30.317359, 30.217359]
+    },
+    slots: {
+      today: [
+        {
+          time: '14:00',
+          isAvailable: false
+        },
+        {
+          time: '15:00',
+          isAvailable: true
+        },
+        {
+          time: '16:00',
+          isAvailable: false
+        },
+      ],
+      tomorrow: [
+        {
+          time: '14:00',
+          isAvailable: false
+        },
+        {
+          time: '15:00',
+          isAvailable: false
+        },
+        {
+          time: '16:00',
+          isAvailable: true
+        },
+      ]
+    }}
+];
